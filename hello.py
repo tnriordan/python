6db07102fdb649ad2822fb0277f7cb890e96e959
Tim <triordan@gmail.com>
2020-08-30 22:59:51 +0000

    Just Goofing Around

---
+++ hello.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+MyString="Hello World"
+print(MyString)
+
+MyKids=[ "Earah","Trinity","Verity","Tessa" ]
+print("The Olest Kid is "+ MyKids[0])
+print("The youngest Kid is "+ MyKids[-1])
+
+MyKids[0]="Earah Yeni"
+print("The Olest Kid is "+ MyKids[0])
+
+MyKidsAges={15,13,8,4}
+
+
